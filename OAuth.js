0f91d30b03633dcb242db64a8946e6ddf4c1c47f
chunk01 <lucamarsman@live.com>
2023-10-18 18:42:14 -0400

    Handle logic for getting accessToken for use in nodemailer

---
+++ OAuth.js
@@ -0,0 +1,14 @@
+const {OAuth2Client} = require('google-auth-library');
+
+const oAuth2Client = new OAuth2Client(process.env.googleapiclient, process.env.googleapisecret); // Intialize OAuth client with login credentials
+
+oAuth2Client.setCredentials({
+    refresh_token: process.env.oauthrefreshtoken 
+});
+    
+async function getOAuthAccessToken() {
+    const accessToken = await oAuth2Client.getAccessToken(); // Get new accesstoken from OAuth
+    return accessToken.token;
+}
+
+module.exports = { getOAuthAccessToken }
