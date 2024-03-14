878d0f31110b20d3ca2498a36080e056b52c3132
chunk01 <lucamarsman@live.com>
2024-03-14 14:27:57 -0400

    Refactor rate limiters to return an error code

--- rateLimiter.js
+++ rateLimiter.js
@@ -3,13 +3,15 @@ const rateLimit = require('express-rate-limit');
 const postLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 15 minutes
     max: 1, // Limit each IP to 1 post every 5 minutes
-    message: 'Too many posts created from this IP, please try again after 5 minutes'
+    message: 'Too many posts created from this IP, please try again after 5 minutes',
+    statusCode: 429
 });
 
 const commentLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
     max: 1, // Limit each IP to 1 comment every minute
-    message: 'Too many comments created from this IP, please try again after 1 minutes'
+    message: 'Too many comments created from this IP, please try again after 1 minutes',
+    statusCode: 429
 });
 
 module.exports = {postLimiter, commentLimiter};
