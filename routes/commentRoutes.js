2c2f01054154fc6a8b4fa9fd867320f2c07dea72
chunk01 <lucamarsman@live.com>
2024-03-14 17:00:05 -0400

    Add multer uplaod as middleware to process formData in the backend

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router(); // create router
 const CommentController = require('../controllers/commentController'); // import comment controller
 const {validateToken} = require('../Auth') // import validateToken middleware
 const {commentLimiter} = require('../rateLimiter');
+const multer = require('multer');
+const upload = multer();
 
-router.post('/post/:postId', commentLimiter, validateToken, CommentController.createComment); // Add Comment to Post
+router.post('/post/:postId', commentLimiter, validateToken, upload.none(), CommentController.createComment);
 router.get("/:postId", CommentController.fetchPostComments); // Fetch Comments for a Post
 router.get("/post-comment-history/:username", CommentController.fetchCommentHistory); // User's Comment History on Posts
 router.post('/reply', commentLimiter, validateToken, CommentController.replyToComment); // Add Comment to Post
